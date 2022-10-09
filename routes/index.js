5252065d36d310a285df78604876f3934d9f03ac
Awasjo <awasjomail@gmail.com>
2022-10-09 04:22:42 -0400

    added view content

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+  res.render('contacts', { title: 'Contact' });
 });
 
 module.exports = router;
