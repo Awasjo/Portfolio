a2b26cf6e573ee842859cee28f8e513227cfdc82
Awasjo <awasjomail@gmail.com>
2022-10-11 22:37:30 -0400

    authentication added

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -2,7 +2,17 @@
 (function(){
     function Start(){
         console.log("App Started...");
+        let deleteButton = document.querySelectorAll('.btn-danger')
+        for(button of deleteButton)
+        {
+            button.addEventListener('click', (event) =>{
+                if(!confirm('Are you sure you want to delete this'))
+                {
+                    event.preventDefault();
+                    window.location.assign("/contact-list");
+                }
+        });
     }
-
+    }
     window.addEventListener("load", Start);
 })();
--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI":"mongodb://127.0.0.1:27017/business_contact"
+    "URI":"mongodb://127.0.0.1:27017/business_contact",
+    "Secret":'SomeSecret'
 }
