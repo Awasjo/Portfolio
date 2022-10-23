71961de153d09ee7a17250b0daae9e436381c311
Awasjo <awasjomail@gmail.com>
2022-10-23 18:47:31 -0400

    updated dbURI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-    "URI":"mongodb://127.0.0.1:27017/business_contact",
+    //"URI":"mongodb://127.0.0.1:27017/business_contact",
+    "URI":"mongodb+srv://Admin:KtruVtyVcE44t5Bz@cluster0.e1cxmrw.mongodb.net/business_contact?retryWrites=true&w=majority",
     "Secret":'SomeSecret'
 }
